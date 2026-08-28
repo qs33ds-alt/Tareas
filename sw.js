5419623167af4f62450a070371700541f9fa5e2a
qs33ds-alt <qs33ds@gmail.com>
2026-08-28 18:32:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE='panel-diario-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='panel-diario-v3';
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim())
+));
 self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).then(r=>{
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
     const copy=r.clone();
     caches.open(CACHE).then(c=>c.put(e.request,copy));
     return r;
