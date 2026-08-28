aa930dbdc0d1e2f8f5246ea9dc9048a3a35c9195
qs33ds-alt <qs33ds@gmail.com>
2026-08-28 18:24:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const CACHE='tareas-voz-v1';
+const CACHE='panel-diario-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
+    return r;
+  }).catch(()=>caches.match(e.request)));
+});
