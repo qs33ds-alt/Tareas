77ffbab152607b34c44635ead0b4d4b68102b10d
qs33ds-alt <qs33ds@gmail.com>
2026-08-30 12:24:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='panel-diario-v3';
+const CACHE='panel-diario-v6';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim())
